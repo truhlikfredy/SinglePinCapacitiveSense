e6b13db7b3691d93752e1a3443c2951537944ac8
Anton Krug <antonkrug@gmail.com>
2020-02-09 05:57:32 +0000

    Easier development/testing

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
+# So the example canbe used to test the library
+ln -s ./examples/PollingThreePins/PollingThreePins.ino SinglePinCapacitiveSense.ino
+
 mkdir build-uno
 mkdir build-due
 mkdir build-nano
 mkdir build-mega
+
+# So the CLI will see in the project's root folder a library with the expected name
 ln -s ./ SinglePinCapacitiveSense
 docker run -it --rm -v $(pwd):/workspace -u $(id -u):$(id -g)  aarontc/arduino-builder arduino-builder -compile -hardware /arduino/hardware -tools /arduino/hardware/tools/avr -tools /arduino/tools-builder -fqbn arduino:avr:uno -libraries /workspace/ -verbose -build-path /workspace/build-uno /workspace/examples/PollingThreePins/PollingThreePins.ino
 docker run -it --rm -v $(pwd):/workspace -u $(id -u):$(id -g)  aarontc/arduino-builder arduino-builder -compile -hardware /arduino/hardware -tools /arduino/hardware/tools/avr -tools /arduino/tools-builder -fqbn arduino:avr:diecimila:cpu=atmega328 -libraries /workspace/ -verbose -build-path /workspace/build-due /workspace/examples/PollingThreePins/PollingThreePins.ino
@@ -9,3 +14,5 @@ docker run -it --rm -v $(pwd):/workspace -u $(id -u):$(id -g)  aarontc/arduino-b
 docker run -it --rm -v $(pwd):/workspace -u $(id -u):$(id -g)  aarontc/arduino-builder arduino-builder -compile -hardware /arduino/hardware -tools /arduino/hardware/tools/avr -tools /arduino/tools-builder -fqbn arduino:avr:mega:cpu=atmega2560 -libraries /workspace/ -verbose -build-path /workspace/build-mega /workspace/examples/PollingThreePins/PollingThreePins.ino
 
 docker run -it --rm -v $(pwd):/workspace -u $(id -u):$(id -g)  aarontc/arduino-builder /arduino/hardware/tools/avr/bin/avr-objdump --source --all-headers --demangle --line-numbers --wide ./build-due/PollingThreePins.ino.elf > ./build-due/map-listing.txt
+
+
